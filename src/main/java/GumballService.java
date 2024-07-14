43e0d44ac8b378915ce5ef50700d89361d57635c
beerth21624 <beerzii.4321@gmail.com>
2024-07-14 19:19:54 +0700

    add name and student number

--- src/main/java/GumballService.java
+++ src/main/java/GumballService.java
@@ -1,51 +1,105 @@
+//6510405822 Sarawut Inpol
 
-
-class GimballService {
-    private enum machine_state{
-        NO_QUARTER,
-        HAS_QUARTER,
-        GIMBAL_SOLD,
-        OUT_OF_GIMBAL
+class GumballService {
+    private enum MachineState {
+        NO_QUARTER, HAS_QUARTER, GUMBALL_SOLD, OUT_OF_GUMBALLS
     }
-    private machine_state state;
+
+    private MachineState state;
     private int inventory;
 
-
-    public GimballService(){
-        state = machine_state.NO_QUARTER;
+    public GumballService() {
+        state = MachineState.NO_QUARTER;
         inventory = 5;
     }
 
+    public int getInventory() {
+        return inventory;
+    }
 
-    public void  insert_quarter(){
-        if(state == machine_state.NO_QUARTER){
-            state = machine_state.HAS_QUARTER;
+    public String getState() {
+        switch (state) {
+            case NO_QUARTER:
+            case HAS_QUARTER:
+                return "Machine is waiting for quarter";
+            case OUT_OF_GUMBALLS:
+                return "Machine is sold out";
+            case GUMBALL_SOLD:
+                return "Gumball is being sold";
+            default:
+                return "";
         }
     }
 
-    public void eject_quarter(){
-        if(state == machine_state.HAS_QUARTER){
-            state = machine_state.NO_QUARTER;
+    public void insertQuarter() {
+        switch (state) {
+            case NO_QUARTER:
+                state = MachineState.HAS_QUARTER;
+                System.out.println("You inserted a quarter");
+                break;
+            case HAS_QUARTER:
+                System.out.println("You can't insert another quarter");
+                break;
+            case OUT_OF_GUMBALLS:
+                System.out.println("You can't insert a quarter, the machine is sold out");
+                break;
+            case GUMBALL_SOLD:
+                System.out.println("Please wait, we're already giving you a gumball");
+                break;
         }
     }
 
-    public void turn_crank(){
-        if(state == machine_state.HAS_QUARTER){
-            state = machine_state.GIMBAL_SOLD;
+    public void ejectQuarter() {
+        switch (state) {
+            case HAS_QUARTER:
+                System.out.println("Quarter returned");
+                state = MachineState.NO_QUARTER;
+                break;
+            case NO_QUARTER:
+                System.out.println("You haven't inserted a quarter");
+                break;
+            case OUT_OF_GUMBALLS:
+            case GUMBALL_SOLD:
+                System.out.println("Sorry, you already turned the crank");
+                break;
         }
     }
 
-    public void dispenss_gimbal(){
-        if(inventory > 0){
+    public void turnCrank() {
+        switch (state) {
+            case HAS_QUARTER:
+                System.out.println("You turned...");
+                state = MachineState.GUMBALL_SOLD;
+                dispenseGumball();
+                break;
+            case NO_QUARTER:
+                System.out.println("You turned but there's no quarter");
+                break;
+            case OUT_OF_GUMBALLS:
+                System.out.println("You turned, but there are no gumballs");
+                break;
+            case GUMBALL_SOLD:
+                System.out.println("Turning twice doesn't get you another gumball!");
+                break;
+        }
+    }
+
+    private void dispenseGumball() {
+        if (inventory > 0) {
+            System.out.println("A gumball comes rolling out the slot");
             inventory--;
-            state = machine_state.NO_QUARTER;
-        }else{
-            state = machine_state.OUT_OF_GIMBAL;
+            if (inventory == 0) {
+                state = MachineState.OUT_OF_GUMBALLS;
+                System.out.println("Oops, out of gumballs!");
+            } else {
+                state = MachineState.NO_QUARTER;
+            }
+        } else {
+            System.out.println("No gumball dispensed");
+            state = MachineState.OUT_OF_GUMBALLS;
         }
     }
 
 }
 
-
-
-
+//6510405822 Sarawut Inpol
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,21 @@
-package main;
+//6510405822 Sarawut Inpol
 
 
 class Main {
     public static void main(String[] args) {
-        const gimbalService = new GimbalService();
+        GumballService machine = new GumballService();
+
+        System.out.println("Sarawut Gumball, Inc.");
+        System.out.println("Java-enabled Standing Gumball Model #2024");
+        System.out.println("Inventory: " + machine.getInventory() + " gumballs");
+        System.out.println(machine.getState());
+        System.out.println();
+
+        machine.insertQuarter();
+        machine.turnCrank();
+        System.out.println();
 
     }
 }
+
+//6510405822 Sarawut Inpol
